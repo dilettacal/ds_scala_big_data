a4ca7a8d3a8536630dffab5df6a9c09ba90c9c01
Di Cal <31538342+dltcls@users.noreply.github.com>
2018-11-09 17:02:29 +0100

    Problem5 finished

--- ExerciseLists/ExerciseLists/src/main/scala/exercise5/Problems.scala
+++ ExerciseLists/ExerciseLists/src/main/scala/exercise5/Problems.scala
@@ -14,26 +14,20 @@ object Problems {
     * multiplyAndFilterEven should multiply all elements of the IntList by
     * the factor x and filter all element that are even
     */
-  def multiplyAndFilterEven(l: IntList, x: Int): IntList = l match {
-    case Empty => l
-    case _ => l.filter(el => el %2 ==0).map(el => el*x)
-  }
+  def multiplyAndFilterEven(l: IntList, x: Int): IntList = l.filter(value => value %2 ==0).map(value => value*x)
 
   /**
     * findMin should find the smallest element of a list
     */
-  def findMin(l: IntList): Int = ???
+  def findMin(l: IntList): Int = l.foldLeft(1)((smallestVal, curVal) => if(curVal < smallestVal) curVal else smallestVal)
 
   /**
     * sumOddNumbers should sum up all odd numbers of a list
     */
-  def sumOddNumbers(l: IntList): Int = l match {
-    case Empty => 0
-    case _ => l.filter(el => el %2 !=0).reduceLeft((x1, x2) => x1+x2)
-  }
+  def sumOddNumbers(l: IntList): Int = l.filter(el => el %2 !=0).reduceLeft((x1, x2) => x1+x2)
 
   /**
     * countEvenNumbers should count all even numbers of a list
     */
-  def countEvenNumbers(l: IntList): Int = ???
+  def countEvenNumbers(l: IntList): Int = l.filter(value => value %2 ==0).foldLeft(0)((counter, curVal) => counter+1)
 }
