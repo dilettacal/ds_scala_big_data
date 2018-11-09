5f9c14279d6df21d036f6afa0d885428ce796dcb
Di Cal <31538342+dltcls@users.noreply.github.com>
2018-11-09 16:28:31 +0100

    Implemented some functions in Problems

--- ExerciseLists/ExerciseLists/src/main/scala/exercise5/Problems.scala
+++ ExerciseLists/ExerciseLists/src/main/scala/exercise5/Problems.scala
@@ -1,8 +1,9 @@
 package exercise5
 
 import list.implementation
+import list.implementation.Empty
 import list.traits.IntList
-;
+
 
 /**
   * Complete the following exercises to practice the usage of higher order functions.
@@ -13,7 +14,10 @@ object Problems {
     * multiplyAndFilterEven should multiply all elements of the IntList by
     * the factor x and filter all element that are even
     */
-  def multiplyAndFilterEven(l: IntList, x: Int): IntList = ???
+  def multiplyAndFilterEven(l: IntList, x: Int): IntList = l match {
+    case Empty => l
+    case _ => l.filter(el => el %2 ==0).map(el => el*x)
+  }
 
   /**
     * findMin should find the smallest element of a list
@@ -23,7 +27,10 @@ object Problems {
   /**
     * sumOddNumbers should sum up all odd numbers of a list
     */
-  def sumOddNumbers(l: IntList): Int = ???
+  def sumOddNumbers(l: IntList): Int = l match {
+    case Empty => 0
+    case _ => l.filter(el => el %2 !=0).reduceLeft((x1, x2) => x1+x2)
+  }
 
   /**
     * countEvenNumbers should count all even numbers of a list
