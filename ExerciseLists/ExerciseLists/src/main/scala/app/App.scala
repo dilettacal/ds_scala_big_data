c60b810e0e17301d1e161b3c4cc427073adc56ac
Di Cal <31538342+dltcls@users.noreply.github.com>
2018-11-15 17:57:29 +0100

    Beispiele ueber fold und reduce

--- ExerciseLists/ExerciseLists/src/main/scala/app/App.scala
+++ ExerciseLists/ExerciseLists/src/main/scala/app/App.scala
@@ -7,5 +7,23 @@ object App extends App {
   val list1 = List(1,2,3,5,7,10,13)
   val lits2 = List("A", "B", "C")
 
+  //apply a binary operator at each element
+  //Result of a step is passed to the next
 
+  println(lits2.reduceLeft(_ + _))
+
+  println(list1.reduceLeft(_ + _))
+
+  println(list1.reduceLeft((x,y) => {println(x + ", " + y); x+y}))
+  println()
+  println(list1.reduceRight((x,y) => {println(x + ", " + y); x+y}))
+  println()
+
+  println(list1.reduceLeft(_ - _))
+  println(list1.reduceRight(_ - _))
+
+  println()
+  println(list1.foldLeft(1)(_ + _))
+  println(lits2.foldLeft("z")(_ + _))
+  println(lits2.foldRight("z")(_ + _))
 }
