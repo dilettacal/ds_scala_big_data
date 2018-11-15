88c825ac633a4de566606e7e1733b6537ac9aaef
Di Cal <31538342+dltcls@users.noreply.github.com>
2018-11-15 18:54:03 +0100

    Lösungen zu Problem5

--- ExerciseLists/ExerciseLists/src/main/scala/exercise5/Problems.scala
+++ ExerciseLists/ExerciseLists/src/main/scala/exercise5/Problems.scala
@@ -14,20 +14,24 @@ object Problems {
     * multiplyAndFilterEven should multiply all elements of the IntList by
     * the factor x and filter all element that are even
     */
-  def multiplyAndFilterEven(l: IntList, x: Int): IntList = l.filter(value => value %2 ==0).map(value => value*x)
+  def multiplyAndFilterEven(l: IntList, x: Int): IntList = l.filter(_ %2 ==0).map(_*x)
 
   /**
     * findMin should find the smallest element of a list
     */
-  def findMin(l: IntList): Int = l.foldLeft(1)((smallestVal, curVal) => if(curVal < smallestVal) curVal else smallestVal)
+  //def findMin(l: IntList): Int = l.foldLeft(1)((smallestVal, curVal) => if(curVal < smallestVal) curVal else smallestVal)
+  def findMin(l: IntList): Int = l.reduceLeft((x, y) => if (y > x) x else y)
+
 
   /**
     * sumOddNumbers should sum up all odd numbers of a list
     */
-  def sumOddNumbers(l: IntList): Int = l.filter(el => el %2 !=0).reduceLeft((x1, x2) => x1+x2)
+ // def sumOddNumbers(l: IntList): Int = l.filter(el => el %2 !=0).reduceLeft((x1, x2) => x1+x2)
+ def sumOddNumbers(l: IntList): Int = l.foldLeft(0)((x, y) => if (y % 2 == 0) x else x + y)
 
   /**
     * countEvenNumbers should count all even numbers of a list
     */
-  def countEvenNumbers(l: IntList): Int = l.filter(value => value %2 ==0).foldLeft(0)((counter, curVal) => counter+1)
+ // def countEvenNumbers(l: IntList): Int = l.filter(value => value %2 ==0).foldLeft(0)((counter, curVal) => counter+1)
+ def countEvenNumbers(l: IntList): Int = l.foldLeft(0)((x, y) => if (y % 2 == 0) x + 1 else x)
 }
